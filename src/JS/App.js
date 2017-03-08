46e48dfacd038d85c1ccddfbcaa7336a98e5d95f
Ryver966 <p.gorski-96@wp.pl>
2017-03-08 16:56:32 +0100

    Changed some styles

--- src/JS/App.js
+++ src/JS/App.js
@@ -12,7 +12,6 @@ class App extends Component {
           <AppHeader />
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
--- src/JS/AppHeader.js
+++ src/JS/AppHeader.js
@@ -5,11 +5,13 @@ export default class AppHeader extends Component {
   render() {
     return (
       <div className='navi-btn-container'>
+        <input type='button' className='sign-in-btn' value='Sign In / Join' />
         <ul className='btn-list'>
           <li>Test 1</li>
           <li>Test 2</li>
           <li>Test 3</li>
           <li>Test 4</li>
+          <li>Test 5</li>
         </ul>
       </div>
     )
