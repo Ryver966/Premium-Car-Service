62272df688522efd46a44d9e6d4b4596d5ee3ca4
Ryver966 <p.gorski-96@wp.pl>
2017-03-09 16:40:40 +0100

    Initialized Firebase

--- src/JS/App.js
+++ src/JS/App.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
+import * as firebase from 'firebase'
 import '../styles/CSS/App.css';
 
 import AppHeader from'./AppHeader/AppHeader';
 
-class App extends Component {
+  var config = {
+    apiKey: "AIzaSyAuadIPLnAcxuzC0C_qMskmht3-PE7sEGA",
+    authDomain: "premium-car-service.firebaseapp.com",
+    databaseURL: "https://premium-car-service.firebaseio.com",
+    storageBucket: "premium-car-service.appspot.com",
+    messagingSenderId: "87651018457"
+  };
+  firebase.initializeApp(config);
+  console.log(firebase);
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    }
+  }
   render() {
     return (
       <div className="App">
@@ -15,4 +32,3 @@ class App extends Component {
   }
 }
 
-export default App;
