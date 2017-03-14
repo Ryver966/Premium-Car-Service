bc52d1049fa53bd20a4da3088953e49623be65ec
Ryver966 <p.gorski-96@wp.pl>
2017-03-14 16:19:47 +0100

    Configures index.js for react-router

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './JS/App';
 import './styles/CSS/index.css';
+import { Router, Route, browserHistory } from 'react-router';
 
 ReactDOM.render(
-  <App />,
+  <Router history={ browserHistory } >
+    <Route path='/' component={ App } />
+  </Router>,
   document.getElementById('root')
 );
