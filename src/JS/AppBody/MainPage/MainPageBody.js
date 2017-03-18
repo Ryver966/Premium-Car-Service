b5fe2dc91267924288506abfcf4184043fa753f3
Ryver966 <p.gorski-96@wp.pl>
2017-03-18 17:17:49 +0100

    Added some sytles to Main Page Body

--- src/JS/AppBody/MainPage/MainPageBody.js
+++ src/JS/AppBody/MainPage/MainPageBody.js
@@ -5,7 +5,17 @@ export default class MainPageBody extends Component {
   render() {
     return(
       <div className='main-page-body'>
-        Test
+        <p className='p1'><h1>Lorem Ipsum</h1>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+          when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+
+        <p className='p2'><h1>Lorem Ipsum</h1>It has survived not only five centuries, but also the leap into electronic typesetting, 
+          remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+          and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        
+        <p className='p3'><h1>Lorem Ipsum</h1>It is a long established fact that a reader will be distracted by the readable content of a page when 
+          looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, 
+          content here', making it look like readable English.</p>
       </div>
     )
   }
